bb8a4b64759febe7703d24da6a426b00c4e71fdc
eduardogamboa11 <eduardo_Gamboa_95@hotmail.com>
2018-09-19 22:52:28 -0500

    switched to numpy

--- CreatePopulation.py
+++ CreatePopulation.py
@@ -1,10 +1,29 @@
+import numpy
 
-def get_population():
-    rows = 10 
-    columns = 10
-    #create random population of 10x10 with 0-255 values for each.
-    population = [[random.randrange(255)]*rows for i in range(columns)]
+def get_population(rows, columns):
+    population = numpy.random.randint(255, size=(rows,columns))
 
-    print(population)
+    return population
 
 
+def tournament(population, columns):
+    number_of_contestants = 3
+    contestants = numpy.random.randint(columns, size=number_of_contestants)
+
+    return min(contestants)
+
+
+def main():
+    rows = 20 
+    columns = 20
+
+    population = get_population(rows, columns)
+
+    father = tournament(population, columns)
+    mother = tournament(population, columns)
+
+    print("Father", population[father])
+    print("Mother", population[mother])
+
+
+main()
